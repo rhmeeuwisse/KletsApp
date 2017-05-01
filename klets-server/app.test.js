38e600ce60f1cf703981f83380be9397d5c8583d
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2017-05-01 10:15:08 +0200

    Tweak app tests suite name

--- klets-server/app.test.js
+++ klets-server/app.test.js
@@ -4,8 +4,8 @@ var app = require('./app');
 
 var testApp = supertest(app);
 
-describe('app', function () {
-    it('Non-existent routes should respond Not found', function (done) {
+describe('route non-existent', function () {
+    it('Should respond Not found', function (done) {
         testApp.get('/non-existent')
             .expect(404)
             .expect('Content-Type', 'text/html; charset=utf-8')
