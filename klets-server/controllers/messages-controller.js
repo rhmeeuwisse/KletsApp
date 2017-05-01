000dfb242a4e17b043cdecde946202bceacc1cc9
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2017-05-01 12:20:20 +0200

    Assert create message body shape

--- klets-server/controllers/messages-controller.js
+++ klets-server/controllers/messages-controller.js
@@ -1,9 +1,21 @@
+var HttpError = require('../lib/http-error');
 var messageRepo = require('../repository/message-repo');
 
 module.exports.create = function (req, res, next) {
     var message = req.body.message;
+    checkDefined(message, 'message');
+    checkDefined(message._id, 'message._id');
+    checkDefined(message.userName, 'message.userName');
+    checkDefined(message.roomName, 'message.roomName');
+    checkDefined(message.text, 'message.text');
 
     messageRepo.save(message);
 
     res.sendStatus(201);
 };
+
+function checkDefined(value, fieldName) {
+    if (typeof value === 'undefined')
+        throw new HttpError(400, 'Missing field: ' + fieldName);
+    return value;
+}
