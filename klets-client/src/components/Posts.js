b361436c513bad2025dcde67fda2876dd9ff4998
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2017-05-01 15:54:57 +0200

    Refactor indentation

--- klets-client/src/components/Posts.js
+++ klets-client/src/components/Posts.js
@@ -2,15 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Posts = ({posts}) => (
-  <ul>
-    {posts.map((post, i) =>
-      <li key={i}>{post.title}</li>
-    )}
-  </ul>
+    <ul>
+        {posts.map((post, i) =>
+            <li key={i}>{post.title}</li>
+        )}
+    </ul>
 )
 
 Posts.propTypes = {
-  posts: PropTypes.array.isRequired
+    posts: PropTypes.array.isRequired
 }
 
 export default Posts
