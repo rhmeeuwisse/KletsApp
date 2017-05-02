22f2d6d0f15fde4393c6197d1ae4e84ace9b7906
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2017-05-02 14:46:47 +0200

    Remove unused imports

--- klets-client/src/containers/App.js
+++ klets-client/src/containers/App.js
@@ -1,39 +1,24 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import React from 'react'
 import Page from '../components/Page'
 import AppHeader from '../components/AppHeader'
 import RoomsPane from '../components/RoomsPane'
-import RoomPane from '../components/MessagesPane'
+import MessagesPane from '../components/MessagesPane'
 import AppFooter from '../components/AppFooter'
 import './Reset.css'
 import '../components/Page.css'
 
-class App extends Component {
-    static propTypes = {
-        selectedRoom: PropTypes.string.isRequired,
-        rooms: PropTypes.array.isRequired,
-    }
+class App extends React.Component {
 
     render() {
         return (
             <Page>
                 <AppHeader />
                 <RoomsPane />
-                <RoomPane />
+                <MessagesPane />
                 <AppFooter />
             </Page>
         )
     }
 }
 
-const mapStateToProps = state => {
-    const {selectedRoom, rooms, roomMessages} = state
-    return {
-        selectedRoom,
-        rooms,
-        roomMessages
-    }
-}
-
-export default connect(mapStateToProps)(App)
+export default App
