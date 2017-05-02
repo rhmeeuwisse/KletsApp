90452cfe0c8ef9e0d52157afc10f1dce4a6ce5fe
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2017-05-02 23:36:18 +0200

    Create new room from RoomCreator

--- klets-client/src/containers/RoomCreator.js
+++ klets-client/src/containers/RoomCreator.js
@@ -1,17 +1,51 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {postRoomMessage, selectRoom} from '../actions'
 
 class RoomSelector extends React.Component {
     static propTypes = {
         /* Connected via react-redux */
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            newRoomName: ''
+        }
+    }
+
+    handleRoomTextChange = (e) => {
+        this.setState({
+            newRoomName: e.target.value
+        })
+    }
+
+    handleCreateRoomClick = (e) => {
+        e.preventDefault();
+        this.handleCreateRoom()
+    }
+
+    handleCreateRoom = () => {
+        const newRoomName = this.state.newRoomName.trim();
+        if (newRoomName.length !== 0) {
+            this.setState({
+                newRoomName: ''
+            });
+            this.props.dispatch(postRoomMessage(newRoomName, 'Klets',
+                this.props.selectedUser + ' created room ' + newRoomName + ' at ' + (new Date().toLocaleTimeString()) + '.'
+            ));
+            this.props.dispatch(selectRoom(newRoomName))
+        }
+    }
+
     render() {
         return (
             <div className="RoomCreator">
-                <button id="RoomCreatorButton">Go</button>
+                <button id="RoomCreatorButton" onClick={this.handleCreateRoomClick}>Go</button>
                 <div id="RoomCreatorInputDiv">
-                    <input type="text" placeholder="Get a room!"/>
+                    <input type="text" placeholder="Get a room!"
+                           value={this.state.newRoomName}
+                           onChange={this.handleRoomTextChange}/>
                 </div>
             </div>
         )
@@ -19,6 +53,7 @@ class RoomSelector extends React.Component {
 }
 
 const mapStateToProps = state => ({
+    selectedUser: state.selectedUser,
     selectedRoom: state.selectedRoom,
     rooms: state.rooms
 })
